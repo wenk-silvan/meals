4c16aca4ed36dc2e962ef66b129bc018e369bfb8
wenksi <silvan.wenk@gmail.com>
2020-01-29 10:21:14 +0100

    Create categories screen and import dummy data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_item.dart';
+import 'package:flutter_complete_guide/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-        
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeals')),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(
+                  category.title,
+                  category.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
