d49bc44ae24c698186f1d151c957d3278b68acc9
wenksi <silvan.wenk@gmail.com>
2020-01-30 09:22:57 +0100

    Add Tabs on top of page in AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/screens/categories_screen.dart';
 import 'package:flutter_complete_guide/screens/category_meals_screen.dart';
 import 'package:flutter_complete_guide/screens/meal_detail_screen.dart';
+import 'package:flutter_complete_guide/screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       /*home: CategoriesScreen(),*/
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.route: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.route: (ctx) => MealDetailScreen(),
       },
